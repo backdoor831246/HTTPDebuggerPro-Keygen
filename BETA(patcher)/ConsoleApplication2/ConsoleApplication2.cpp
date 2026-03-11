b772f5145e93f5e59ab316818cb70c5b531ed047
minhara. <ivanovskiy.davidik@gmail.com>
2026-03-11 08:29:26 +0200

    Add files via upload

---
+++ BETA(patcher)/ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,95 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <cstdint>
+#include <cstring>
+
+// sub_4218F0 - trial days check, patch to always return 30
+static const uint8_t g_pattern[] = {
+	0x55, 0x8B, 0xEC, 0x83, 0xE4, 0xF8, 0x81, 0xEC,
+	0x38, 0x04, 0x00, 0x00, 0xA1, 0x44, 0x4E, 0xA3,
+	0x00, 0x33, 0xC4, 0x89, 0x84, 0x24, 0x34, 0x04,
+	0x00, 0x00, 0x56, 0x57, 0x6A, 0x7C, 0x0F, 0x57
+};
+static const size_t g_pattern_size = sizeof(g_pattern);
+
+// mov eax, 30 ; ret
+static const uint8_t g_patch[] = { 0xB8, 0x1E, 0x00, 0x00, 0x00, 0xC3 };
+static const size_t  g_patch_size = sizeof(g_patch);
+
+static const char    g_marker[] = "The evaluation period has expired.";
+static const size_t  g_marker_len = sizeof(g_marker) - 1;
+
+int main(int argc, char* argv[])
+{
+	if (argc < 2) {
+		std::cerr << "234r.exe 34234.exe\n";
+		return 1;
+	}
+
+	std::ifstream fin(argv[1], std::ios::binary);
+	if (!fin) {
+		std::cerr << "error: cannot open " << argv[1] << "\n";
+		return 1;
+	}
+
+	std::vector<uint8_t> buf(
+		(std::istreambuf_iterator<char>(fin)),
+		(std::istreambuf_iterator<char>()));
+	fin.close();
+
+	if (buf.size() < 2 || buf[0] != 'M' || buf[1] != 'Z') {
+		std::cerr << "error: not a valid PE\n";
+		return 1;
+	}
+
+	bool marker_found = false;
+	for (size_t i = 0; i + g_marker_len <= buf.size(); i++) {
+		if (memcmp(buf.data() + i, g_marker, g_marker_len) == 0) {
+			marker_found = true;
+			break;
+		}
+	}
+	if (!marker_found) {
+		std::cerr << "error: target binary not recognized\n";
+		return 1;
+	}
+
+	int count = 0;
+	for (size_t i = 0; i + g_pattern_size <= buf.size(); i++) {
+		if (memcmp(buf.data() + i, g_pattern, g_pattern_size) == 0) {
+			printf("found   : 0x%X\n", (unsigned int)i);
+			printf("before  : ");
+			for (size_t j = 0; j < g_patch_size; j++) printf("%02X ", buf[i + j]);
+			printf("\n");
+
+			memcpy(buf.data() + i, g_patch, g_patch_size);
+
+			printf("after   : ");
+			for (size_t j = 0; j < g_patch_size; j++) printf("%02X ", g_patch[j]);
+			printf("\n\n");
+
+			count++;
+		}
+	}
+
+	if (count == 0) {
+		std::cerr << "error: pattern not found\n";
+		return 1;
+	}
+
+	printf("patched : %d occurrence(s)\n\n", count);
+
+	std::string out = std::string(argv[1]) + ".patched.exe";
+	std::ofstream fout(out, std::ios::binary);
+	if (!fout) {
+		std::cerr << "error: cannot write " << out << "\n";
+		return 1;
+	}
+	fout.write(reinterpret_cast<const char*>(buf.data()), buf.size());
+	fout.close();
+
+	std::cout << "done -> " << out << "\n";
+	return 0;
+}
---
+++ HTTP Debugger Pro - KeyGen/HTTP Debugger Pro - KeyGen.cpp
@@ -0,0 +1,108 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <windows.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+static void printTime(void) {
+	SYSTEMTIME st;
+	GetLocalTime(&st);
+	printf("[%02d:%02d:%02d] ", st.wHour, st.wMinute, st.wSecond);
+}
+
+static void logInfo(const char *label, const char *val) {
+	printTime();
+	printf("%s%s\n", label, val);
+}
+
+static void logOk(const char *msg) {
+	printTime();
+	printf("%s\n", msg);
+}
+
+static void logErr(const char *msg) {
+	printTime();
+	printf("%s\n", msg);
+}
+
+int main(void) {
+	HKEY hKey;
+	char appVer[256] = { 0 };
+	DWORD bufSize = sizeof(appVer);
+
+	if (RegOpenKeyExA(HKEY_CURRENT_USER,
+		"Software\\MadeForNet\\HTTPDebuggerPro",
+		0, KEY_READ, &hKey) != ERROR_SUCCESS)
+	{
+		logErr("HTTP Debugger Pro installation not found");
+		printf("\n  press any key to close\n");
+		getchar(); return 1;
+	}
+	RegQueryValueExA(hKey, "AppVer", NULL, NULL, (LPBYTE)appVer, &bufSize);
+	RegCloseKey(hKey);
+
+	logInfo("detected  : ", appVer);
+
+	char *lastSpace = strrchr(appVer, ' ');
+	if (!lastSpace) { logErr("version parse failed"); getchar(); return 1; }
+
+	char verDigits[64] = { 0 };
+	int di = 0;
+	for (int i = 1; lastSpace[i]; i++)
+		if (lastSpace[i] != '.') verDigits[di++] = lastSpace[i];
+
+	DWORD version = (DWORD)atol(verDigits);
+	{
+		char tmp[32]; sprintf(tmp, "%lu", version);
+		logInfo("version   : ", tmp);
+	}
+
+	DWORD serial = 0;
+	GetVolumeInformationA("C:\\", NULL, 0, &serial, NULL, NULL, NULL, 0);
+	{
+		char tmp[32]; sprintf(tmp, "0x%08X", serial);
+		logInfo("hardware  : ", tmp);
+	}
+
+	DWORD snIndex = version ^ (((~serial) >> 1) + 736) ^ 0x590D4;
+	char valueName[64] = { 0 };
+	sprintf(valueName, "SN%lu", snIndex);
+	logInfo("key name  : ", valueName);
+
+	srand(GetTickCount());
+	unsigned char v1 = (unsigned char)(rand() % 255);
+	unsigned char v2 = (unsigned char)(rand() % 255);
+	unsigned char v3 = (unsigned char)(rand() % 255);
+
+	char licKey[20] = { 0 };
+	sprintf(licKey, "%02X%02X%02X7C%02X%02X%02X%02X",
+		(unsigned int)v1,
+		(unsigned int)(v2 ^ 0x7C),
+		(unsigned int)((~v1) & 0xFF),
+		(unsigned int)v2,
+		(unsigned int)(v3 % 255),
+		(unsigned int)((v3 % 255) ^ 7),
+		(unsigned int)(v1 ^ ((~(v3 % 255)) & 0xFF)));
+
+	logInfo("license   : ", licKey);
+
+	DWORD dwDisp;
+	if (RegCreateKeyExA(HKEY_CURRENT_USER,
+		"Software\\MadeForNet\\HTTPDebuggerPro",
+		0, NULL, 0, KEY_ALL_ACCESS, NULL, &hKey, &dwDisp) == ERROR_SUCCESS)
+	{
+		RegSetValueExA(hKey, valueName, 0, REG_SZ,
+			(const BYTE*)licKey, (DWORD)strlen(licKey) + 1);
+		RegCloseKey(hKey);
+		printf("\n");
+		logOk("done");
+	}
+	else
+	{
+		logErr("registry write failed");
+	}
+
+	printf("\n  press any key to close\n");
+	getchar();
+	return 0;
+}
